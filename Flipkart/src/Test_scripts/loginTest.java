10815465af15f1306850fe7212668c76c97888ab
sumaobanna <84015415+sumaobanna@users.noreply.github.com>
2021-05-11 21:26:42 +0530

    changes

--- Flipkart/src/Test_scripts/loginTest.java
+++ Flipkart/src/Test_scripts/loginTest.java
@@ -1,5 +1,33 @@
 package Test_scripts;
+import org.testng.annotations.Test;
 
-public class loginTest {
+import generic.Generic;
+import junit.framework.Assert;
+import pom.LoginPage;
+
+public class loginTest extends Generic {
+  LoginPage lp=new LoginPage(d);
+	
+	@Test
+	public void login() throws Exception
+	{
+		lp.setusername("suma");
+		lp.setpwd("****");
+		lp.clicklogin();
+		String title = d.getCurrentUrl();
+		Assert.assertTrue(title.contains("https://www.flipkart.com/")); 
+		
+	}
+	
+	@Test
+	public void validateerrorMSGInlogin() throws Exception
+	{
+		lp.setusername("suma");
+		lp.setpwd("****");
+		lp.clicklogin();
+		lp.geterrorMsg(); 
+		
+	}
+  
 
 }
